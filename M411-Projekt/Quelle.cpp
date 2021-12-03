84b5b7756eaf32b8be9d9f9124214027525fc1b8
Ben Streich <ben.streich@icloud.com>
2021-12-03 13:45:45 +0100

    x

--- M411-Projekt/Quelle.cpp
+++ M411-Projekt/Quelle.cpp
@@ -1,15 +1,19 @@
 ﻿#include <stdlib.h>
 #include <stdio.h>
 
-int main()
-{
-	printf("Diese welt sie gehört den juden nicht");
-}
-
-
 typedef struct Cards {
 	char Name[30];
 	int Wert1;
 	double Wert2;
 	struct Cards* pNext;
 }structCards;
+
+
+
+
+int main()
+{
+	printf("Diese welt sie gehört den juden nicht");
+}
+
+
