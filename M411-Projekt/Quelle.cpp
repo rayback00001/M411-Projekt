922842bf4ab5bdf82d83457f9da8b8aa78717b08
Ben Streich <ben.streich@icloud.com>
2021-12-03 13:55:48 +0100

    x

--- M411-Projekt/Quelle.cpp
+++ M411-Projekt/Quelle.cpp
@@ -14,6 +14,7 @@ structCards ListofCards();
 int main()
 {
 	printf("Diese welt sie gehört den juden nicht");
+	printf("Kek = ", ListofCards());
 }
 
 structCards ListofCards() {
@@ -136,4 +137,6 @@ structCards ListofCards() {
 
 	//nächstes Element
 	pCard10->pNext = NULL;
+
+	pStart->pNext = pCard10;
 }
